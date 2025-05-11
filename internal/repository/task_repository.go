f8710f6688b6b1a4d819b4a3519dee48396fecf3
Fardan Nozami Ajitama <fardan.nozami@gmail.com>
2025-05-11 16:09:06 +0700

    feat: get all, delete and mark as complete repository

--- internal/repository/task_repository.go
+++ internal/repository/task_repository.go
@@ -1,6 +1,11 @@
 package repository
 
-import "github.com/fardannozami/golang-todolist-cli/internal/model"
+import (
+	"fmt"
+	"time"
+
+	"github.com/fardannozami/golang-todolist-cli/internal/model"
+)
 
 type TaskRepository interface {
 	AddTask(task model.Task) error
@@ -22,4 +27,31 @@ func NewInMemoryTaskRepository() *InMemoryTaskRepository {
 func (r *InMemoryTaskRepository) AddTask(task model.Task) error {
 	r.tasks = append(r.tasks, task)
 	return nil
+}
+
+func (r *InMemoryTaskRepository) GetAllTasks() ([]model.Task, error) {
+	return r.tasks, nil
+}
+
+func (r *InMemoryTaskRepository) DeleteTask(id int) error {
+	for i, task := range r.tasks {
+		if task.Id == id {
+			r.tasks = append(r.tasks[:i], r.tasks[i+1:]...)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("task with id %d not found", id)
+}
+
+func (r *InMemoryTaskRepository) MarkTaskAsCompleted(id int) error {
+	for i, task := range r.tasks {
+		if task.Id == id {
+			completed := time.Now()
+			r.tasks[i].CompletedAt = &completed
+			return nil
+		}
+	}
+
+	return fmt.Errorf("task with id %d not found", id)
 }
--- internal/repository/task_repository_test.go
+++ internal/repository/task_repository_test.go
@@ -8,18 +8,111 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestInMemoryTaskRepository_AddTask(t *testing.T) {
-	task := model.Task{
+var tasks = []model.Task{
+	{
 		Id:          1,
-		Description: "Test Task",
+		Description: "Test Task 1",
 		CreatedAt:   time.Now(),
-	}
+	},
+	{
+		Id:          2,
+		Description: "Test Task 2",
+		CreatedAt:   time.Now(),
+	},
+}
 
+func TestInMemoryTaskRepository_AddTask(t *testing.T) {
 	repo := NewInMemoryTaskRepository()
-	err := repo.AddTask(task)
-	if err != nil {
-		t.Errorf("AddTask() error = %v", err)
+	for _, task := range tasks {
+		err := repo.AddTask(task)
+		if err != nil {
+			t.Errorf("AddTask() error = %v", err)
+		}
 	}
 
+	assert.Len(t, repo.tasks, 2)
+	assert.Equal(t, tasks[0], repo.tasks[0])
+}
+
+func TestInMemoryTaskRepository_GetTasks(t *testing.T) {
+	repo := NewInMemoryTaskRepository()
+	for _, task := range tasks {
+		err := repo.AddTask(task)
+		if err != nil {
+			t.Errorf("AddTask() error = %v", err)
+		}
+	}
+
+	tasks, err := repo.GetAllTasks()
+	if err != nil {
+		t.Errorf("GetTasks() error = %v", err)
+	}
+
+	assert.NoError(t, err)
+	assert.Len(t, tasks, 2)
+}
+
+func TestInMemoryTaskRepository_DeleteTask(t *testing.T) {
+	repo := NewInMemoryTaskRepository()
+	for _, task := range tasks {
+		err := repo.AddTask(task)
+		if err != nil {
+			t.Errorf("AddTask() error = %v", err)
+		}
+	}
+
+	err := repo.DeleteTask(1)
+	if err != nil {
+		t.Errorf("DeleteTask() error = %v", err)
+	}
+	assert.NoError(t, err)
 	assert.Len(t, repo.tasks, 1)
+	assert.Equal(t, tasks[1], repo.tasks[0])
+}
+
+func TestInMemoryTaskRepository_DeleteTaskNotFound(t *testing.T) {
+	repo := NewInMemoryTaskRepository()
+	for _, task := range tasks {
+		err := repo.AddTask(task)
+		if err != nil {
+			t.Errorf("AddTask() error = %v", err)
+		}
+	}
+
+	err := repo.DeleteTask(900)
+	assert.Error(t, err)
+	assert.Len(t, repo.tasks, 2)
+}
+
+func TestInMemoryTaskRepository_MarkTaskAsCompleted(t *testing.T) {
+	repo := NewInMemoryTaskRepository()
+	for _, task := range tasks {
+		err := repo.AddTask(task)
+		if err != nil {
+			t.Errorf("AddTask() error = %v", err)
+		}
+	}
+
+	err := repo.MarkTaskAsCompleted(1)
+	if err != nil {
+		t.Errorf("MarkTaskAsCompleted() error = %v", err)
+	}
+	assert.NoError(t, err)
+	assert.NotNil(t, repo.tasks[0].CompletedAt)
+	assert.Nil(t, repo.tasks[1].CompletedAt)
+}
+
+func TestInMemoryTaskRepository_MarkTaskAsCompletedNotFound(t *testing.T) {
+	repo := NewInMemoryTaskRepository()
+	for _, task := range tasks {
+		err := repo.AddTask(task)
+		if err != nil {
+			t.Errorf("AddTask() error = %v", err)
+		}
+	}
+
+	err := repo.MarkTaskAsCompleted(100)
+	assert.Error(t, err)
+	assert.Nil(t, repo.tasks[0].CompletedAt)
+	assert.Nil(t, repo.tasks[1].CompletedAt)
 }
